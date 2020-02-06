bb997bf4826141f766eb3bb4649005cc1a061090
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-02-06 18:31:03 -0500

    created four static methods

--- Assignment_Lab3/Program.cs
+++ Assignment_Lab3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Assignment_Lab3
 {
@@ -13,7 +14,41 @@ namespace Assignment_Lab3
             Console.WriteLine("\nWelcome to Nazif Sahim Multi Threading Calculator");
             Console.WriteLine("---------------------------------------------------");
 
+            while (a != -1)
+            {
+                try
+                {
+                    Console.WriteLine("\nPlease enter first number or -1 to discontinue: ");
+                    a = double.Parse(Console.ReadLine());
+                    if (a == 0)
+                    {
+
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please Check Your Input !");
+                }
+            }
             
         }
+    }
+
+    public static void Add()
+    {
+        Thread.Sleep(3000);
+
+    }
+    public static void Subtract()
+    {
+
+    }
+    public static void Multiply()
+    {
+
+    }
+    public static void Divide()
+    {
+
     }
 }
