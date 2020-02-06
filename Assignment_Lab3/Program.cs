0ac1c1430e40b59853d8a7e9c99eae371f97ea13
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-02-06 18:55:50 -0500

    calculation result in each static method

--- Assignment_Lab3/Program.cs
+++ Assignment_Lab3/Program.cs
@@ -18,7 +18,7 @@ namespace Assignment_Lab3
             {
                 try
                 {
-                    Console.WriteLine("\nPlease enter first number or -1 to discontinue: ");
+                    Console.Write("\nPlease enter first number or -1 to discontinue: ");
                     a = double.Parse(Console.ReadLine());
                     if (a == 0)
                     {
@@ -31,7 +31,7 @@ namespace Assignment_Lab3
                         Console.ReadLine();
                         break;
                     }
-                    Console.WriteLine("\nPlease enter second number: ");
+                    Console.Write("\nPlease enter second number: ");
                     b = double.Parse(Console.ReadLine());
 
                     Thread addition = new Thread(new ThreadStart(Add));
@@ -40,6 +40,7 @@ namespace Assignment_Lab3
                     Thread division = new Thread(() => Divide());
                     Console.WriteLine("\nYour Input Calculation Results are");
                     Console.WriteLine("------------------------------------");
+
                 }
                 catch (Exception)
                 {
@@ -57,14 +58,24 @@ namespace Assignment_Lab3
         }
         public static void Subtract()
         {
+            Thread.Sleep(5000);
+            result = a - b;
+            Console.WriteLine("\nThe result of subtraction is :" + result);
 
         }
         public static void Multiply()
         {
+            Thread.Sleep(8000);
+            result = a * b;
+            Console.WriteLine("\nThe result of multipication is :" + result);
 
         }
         public static void Divide()
         {
+            Thread.Sleep(11000);
+            result = a + b;
+            Console.WriteLine("\nThe result of division is :" + result);
+            Console.WriteLine("\nEnd....");
 
         }
     }
