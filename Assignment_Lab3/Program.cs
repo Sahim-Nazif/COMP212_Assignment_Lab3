852a1c0df9e3c9eacf890de8bf9e56e7d4b0b4ff
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-02-06 18:24:10 -0500

    created local static variables

--- Assignment_Lab3/Program.cs
+++ Assignment_Lab3/Program.cs
@@ -4,8 +4,15 @@ namespace Assignment_Lab3
 {
     class Program
     {
+        //creating local variables
+        public static double a;
+        public static double b;
+        public static double result;
         static void Main(string[] args)
         {
+            Console.WriteLine("\nWelcome to Nazif Sahim Multi Threading Calculator");
+            Console.WriteLine("---------------------------------------------------");
+
             
         }
     }
