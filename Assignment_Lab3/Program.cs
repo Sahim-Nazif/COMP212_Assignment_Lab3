9ed0061b16542d2357801b43e73ea70832748f98
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-02-06 19:48:17 -0500

    added stopwatch to count the miliseconds elapsed for each thread

--- Assignment_Lab3/Program.cs
+++ Assignment_Lab3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace Assignment_Lab3
@@ -9,6 +10,7 @@ namespace Assignment_Lab3
         public static double a;
         public static double b;
         public static double result;
+        
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to Nazif Sahim Multi Threading Calculator");
@@ -20,16 +22,17 @@ namespace Assignment_Lab3
                 {
                     Console.Write("\nPlease enter first number or -1 to discontinue: ");
                     a = double.Parse(Console.ReadLine());
-                    if (a == 0 || a<=0)
+               
+                    if (a ==-1)
+                    {
+                        Console.WriteLine("You have chosen to exit the application. Bye now ");
+                        Console.ReadLine();
+                        break;
+                    }
+                   else if (a == 0 || a < 0)
                     {
                         Console.WriteLine("The value which you have entered is not valid for division.. Please enter a value except zeor !");
                         continue;
-                    }
-                    if (a == -1)
-                    {
-                        Console.WriteLine("You have chose to exit the application. Bye now ");
-                        Console.ReadLine();
-                        break;
                     }
                     Console.Write("\nPlease enter second number: ");
                     b = double.Parse(Console.ReadLine());
@@ -46,6 +49,7 @@ namespace Assignment_Lab3
                     division.Start();
                     Console.ReadLine();
 
+                  
                 }
                 catch (Exception)
                 {
@@ -56,30 +60,44 @@ namespace Assignment_Lab3
         }
         public static void Add()
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             Thread.Sleep(3000);
             result = a + b;
             Console.WriteLine("\nThe result of addition is :" + result);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds + " miliseconds elapsed to perform addition");
 
         }
         public static void Subtract()
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             Thread.Sleep(5000);
             result = a - b;
             Console.WriteLine("\nThe result of subtraction is :" + result);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds + " miliseconds elapsed to perform subtraction");
 
         }
         public static void Multiply()
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             Thread.Sleep(8000);
             result = a * b;
             Console.WriteLine("\nThe result of multipication is :" + result);
+            stopwatch.Stop();
+            
+            Console.WriteLine(stopwatch.ElapsedMilliseconds + " miliseconds elapsed to perform multipication");
 
         }
         public static void Divide()
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             Thread.Sleep(11000);
             result = a / b;
             Console.WriteLine("\nThe result of division is :" + result);
+            stopwatch.Stop();
+            
+            Console.WriteLine(stopwatch.ElapsedMilliseconds + " miliseconds elapsed to peform division");
             Console.WriteLine("\nEnd....");
 
         }
